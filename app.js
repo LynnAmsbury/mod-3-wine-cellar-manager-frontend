318c8e52924a536f65ded02cda20c0f99f88b4e5
LynnAmsbury <Lynn8542@gmail.com>
2020-07-09 15:05:25 -0600

    Cleaned up file structure; added form to index.html for adding new wines

--- app.js
+++ app.js
@@ -51,63 +51,7 @@ function displayWinesList(wines) {
     wines.map(wine => {
         // console.log(wine)
         const wineData = document.createElement('h3');
-        wineData.innerHTML = `<a href=showWine.html?id=${wine.id}>${wine.varietal}, ${wine.producer}, ${wine.region}, ${wine.vintage}</a>`
+        wineData.innerHTML = `<a href=showWine/showWine.html?id=${wine.id}>${wine.varietal}, ${wine.producer}, ${wine.region}, ${wine.vintage}</a>`
         winesList.append(wineData);
     })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Functionality to display the index wines list as a list of cards
-function showWineCards(wines) {
-    // console.log('wines:', wines)
-    const winesCardSection = document.getElementById('wine-card-section');
-    wines.forEach((wine) => showWine(wine, winesCardSection));
-}
-
-function showWine(wine, winesCardSection) {
-    const wineCard = document.createElement('div');
-    wineCard.classList.add('wine-card');
-
-    const varietal = document.createElement('h2');
-    varietal.textContent = wine.varietal;
-
-    const producer = document.createElement('h4');
-    producer.textContent = wine.producer;
-
-    const region = document.createElement('h4');
-    region.textContent = wine.region;
-
-    const vintage = document.createElement('h4');
-    vintage.textContent = wine.vintage;
-
-    const numberInCollection = document.createElement('h4');
-    numberInCollection.textContent = wine.number_in_collection;
-
-
-    wineCard.append(varietal, producer, region, vintage, numberInCollection);
-
-
-    winesCardSection.append(wineCard)
 }
