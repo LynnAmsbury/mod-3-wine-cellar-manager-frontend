6bc09d7ee2a4e7c8cfcf20433894a8598bb58d81
LynnAmsbury <Lynn8542@gmail.com>
2020-07-10 13:24:31 -0600

    Debugged error in app.js; fixed code that wasn't displaying the main varietals list and the wines list appropriately

--- app.js
+++ app.js
@@ -9,14 +9,25 @@ function getAllWines() {
     return fetch(winesURL)
         .then(parseJSON)
 }
+
 getAllWines().then((response) => {
     varietalsList(response);
 })
 
 const varietalsListUl = document.getElementById("varietals-list-ul")
-
+function getDeduplicatedWines(wines, propertyToDeduplicateBy) {
+    return wines.reduce((accumulator, currentWine) => {
+        const matches = accumulator.filter(currentWineInAccumulator => {
+            return currentWineInAccumulator[propertyToDeduplicateBy] === currentWine[propertyToDeduplicateBy];
+        });
+        return matches.length ?
+          accumulator :
+          accumulator.concat(currentWine);
+    }, []);
+}
 function varietalsList(wines) {
-    return wines.forEach(wine => {
+    const deduplicatedWines = getDeduplicatedWines(wines, 'varietal');
+    return deduplicatedWines.forEach(wine => {
         const li=document.createElement("li")
         const button = document.createElement("button")
         button.textContent = wine.varietal
@@ -28,31 +39,92 @@ function varietalsList(wines) {
         varietalsListUl.append(li)
     })
 }
-
 function showWineList(event) {
     fetch(winesURL)
         .then(parseJSON)
         .then(result => sortWinesList(result, event.target.innerText));
 }
-
 function sortWinesList(allWinesList, varietal) {
-
     const sortedWineList = allWinesList.filter(wine => {
         if (wine.varietal == varietal) {
             return wine
         }
-        
     })
+    console.log(allWinesList, sortedWineList)
     displayWinesList(sortedWineList)
 }
-
 const winesList = document.getElementById('wines-list-ul');
-
+// Functionality to display indes wines list as a ul list
 function displayWinesList(wines) {
+    // console.log(wines)
+    winesList.innerHTML = '';
     wines.map(wine => {
+        // console.log(wine)
         const wineData = document.createElement('li');
         wineData.innerHTML = `<a href=showWine/showWine.html?id=${wine.id}>${wine.varietal}, ${wine.producer}, ${wine.region}, ${wine.vintage}</a>`
-        winesList.innerHTML = '';
         winesList.append(wineData);
     })
 }
+
+
+
+
+
+// const baseURL = 'http://localhost:7000';
+// const winesURL = `${baseURL}/wines`;
+
+// function parseJSON(response) {
+//     return response.json();
+// }
+
+// function getAllWines() {
+//     return fetch(winesURL)
+//         .then(parseJSON)
+// }
+// getAllWines().then((response) => {
+//     varietalsList(response);
+// })
+
+// const varietalsListUl = document.getElementById("varietals-list-ul")
+
+// function varietalsList(wines) {
+//     return wines.forEach(wine => {
+//         const li=document.createElement("li")
+//         const button = document.createElement("button")
+//         button.textContent = wine.varietal
+//         button.classList.add("de-button")
+//         li.appendChild(button);
+//         li.addEventListener("click", (event) => {
+//             showWineList(event)
+//         })
+//         varietalsListUl.append(li)
+//     })
+// }
+
+// function showWineList(event) {
+//     fetch(winesURL)
+//         .then(parseJSON)
+//         .then(result => sortWinesList(result, event.target.innerText));
+// }
+
+// function sortWinesList(allWinesList, varietal) {
+
+//     const sortedWineList = allWinesList.filter(wine => {
+//         if (wine.varietal == varietal) {
+//             return wine
+//         }
+        
+//     })
+//     displayWinesList(sortedWineList)
+// }
+
+// const winesList = document.getElementById('wines-list-ul');
+
+// function displayWinesList(wines) {
+//     wines.map(wine => {
+//         const wineData = document.createElement('li');
+//         wineData.innerHTML = `<a href=showWine/showWine.html?id=${wine.id}>${wine.varietal}, ${wine.producer}, ${wine.region}, ${wine.vintage}</a>`
+//         winesList.innerHTML = '';
+//         winesList.append(wineData);
+//     })
+// }
